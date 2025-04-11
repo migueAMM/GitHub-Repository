56b6d252c008822dacb1b29e2430c36f0d69772c
migueAMM <166752912+migueamm@users.noreply.github.com>
2025-04-10 23:54:06 -0500

    class listaEnlazada with a for from 0 to 10 saying hello Miugel + i

--- listasEnlazadasED/src/Main.java
+++ listasEnlazadasED/src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
         }
     }
 }
